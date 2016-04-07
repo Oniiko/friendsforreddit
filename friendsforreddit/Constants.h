c1122f8affa015fc8dd81baad492fd3ddfd0f647
Gregory Johnson <gregoryhjohnson@172-16-188-240.dynapool.nyu.edu>
2016-04-07 11:54:13 -0400

    Added some useful constants

---
+++ friendsforreddit/Constants.h
@@ -0,0 +1,20 @@
+//
+//  Constants.h
+//  friendsforreddit
+//
+//  Created by Gregory Johnson on 4/5/16.
+//  Copyright © 2016 nyu.edu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+extern NSString * const BaseURL; //For making API calls via OAuth once logged in
+extern NSString * const ClientID; //Our client ID
+extern NSString * const BaseAuthorizationURL; //For redirecting user to authorization site
+extern NSString * const BaseAccessURL; //For obtaining an access token
+extern NSString * const UserAgent;
+
+
+@end
---
+++ friendsforreddit/Constants.m
@@ -0,0 +1,20 @@
+//
+//  Constants.m
+//  friendsforreddit
+//
+//  Created by Gregory Johnson on 4/5/16.
+//  Copyright © 2016 nyu.edu. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+ NSString * const BaseOAuthURL = @"https://oauth.reddit.com/";
+ NSString * const ClientID = @"X74G4QWSkO_ofA";
+ NSString * const BaseAuthorizationURL = @"https://www.reddit.com/api/v1/authorize/";
+ NSString * const BaseAccessURL = @"https://www.reddit.com/api/v1/access_token/";
+ NSString * const UserAgent = @"FriendsForReddit/0.1 by friends_for_reddit";
+
+
+@end
