3b3e34fad8ad408384ffd1c505e2bafcf5fb005f
Nicholas <nicholas.wen@gmail.com>
2016-05-02 12:40:38 -0400

    Conditional View and Web View Resizing

--- friendsforreddit/FirstViewController.m
+++ friendsforreddit/FirstViewController.m
@@ -13,7 +13,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIColor *redditBlue = [UIColor colorWithRed:206.0/255.0 green:227.0/255.0 blue:248.0/255.0 alpha:1];
+    [[UINavigationBar appearance] setBarTintColor:redditBlue];
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"access_token"];
 
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    if ([[NSUserDefaults standardUserDefaults] stringForKey:@"access_token"]) {
+        [self performSegueWithIdentifier:@"loggedInSegue" sender:nil];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
