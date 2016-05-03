31e2ea96e9e09cdeea554932f7d274e93a689eb7
Nicholas <nicholas.wen@gmail.com>
2016-05-03 16:45:37 -0400

    Visual Update

--- friendsforreddit/LogInViewController.h
+++ friendsforreddit/LogInViewController.h
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.h
+//  LoginViewController.h
 //  friendsforreddit
 //
 //  Created by Gregory Johnson on 4/5/16.
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FirstViewController : UIViewController
+@interface LogInViewController : UIViewController
 {
 }
 
